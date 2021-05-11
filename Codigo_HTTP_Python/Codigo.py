8e8a09e47e3b4720d476c97e8122d8373348221c
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-05-10 23:58:36 -0300

    Update Codigo.py

--- Codigo_HTTP_Python/Codigo.py
+++ Codigo_HTTP_Python/Codigo.py
@@ -5,11 +5,14 @@
 
 import web
 import json
+import http.client
+import pymssql
 
 urls = (
     '/', 'padrao',
     '/health', 'health',
-    '/mail/validation/v1', 'validacao'
+    '/mail/validation/v1', 'validacaov1',
+    '/mail/validation/v3', 'validacaov3'
 )
 
 app = web.application(urls, globals())
@@ -24,29 +27,40 @@ class padrao:
 
 class health:        
     def GET(self):
-        return json.dumps({
-            'status': "OK",
-            'code': 200,
-            'results':[{"message": "Servidor executando na porta 8080"}]'
-        })
+        return json.dumps({'status':"OK",
+                          'code': 200,
+                          'results': [{"message": "Servidor executando na porta 8080"}]
+                          })
 
-class validacao:        
+class validacaov1:        
     def POST(self):
         jsonbody = json.loads(web.data())
         emailsvalidados = []
         for email in jsonbody['emails']:
-            emailsvalidados.append(json.loads(validaremail(email)))
+            emailvalidado = json.loads(validaremail(email))
+            inseriremailvalidadov1(emailvalidado)
+            emailsvalidados.append(emailvalidado)
         return json.dumps({
             'status': 'OK',
             'code': 200,
             'results': emailsvalidados})
 
+def inseriremailvalidadov1(email):
+    conn = pymssql.connect(server='localhost', user='sa', password='password', database='Residuall')  
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO ValidacaoV1 (email_address, domain, valid_syntax) VALUES (%s, %s, %s)",
+        (email['email_address'], email['domain'], email['valid_syntax']))
+    conn.commit()
+    cursor.close()
+    conn.close()
+   
 def validaremail(email):
     email_address = str(email['email_address'])
     valido = False
-    if (email_address.endswith(".com")):
+    if (email_address.endswith(".com.br")):
         valido = True
-    elif (email_address.endswith(".com.br")):
+    elif (email_address.endswith(".com")):
         valido = True
     elif (email_address.endswith(".gov.br")):
         valido = True
@@ -57,6 +71,27 @@ def validaremail(email):
             'domain' : "email",
             'valid_syntax' : valido
             })
+
+class validacaov3:
+    def POST(self):
+        conn = http.client.HTTPSConnection("api.eva.pingutil.com")
+        payload = ''
+        headers = {}
+
+        jsonbody = json.loads(web.data())
+        emailsvalidados = []
+        for email in jsonbody['emails']:
+            email_address = str(email['email_address'])
+            conn.request("GET", "/email?email=" + email_address,
+                         payload,
+                         headers)
+            res = json.loads(conn.getresponse().read())
+            emailsvalidados.append({'data': res['data']})
+        return json.dumps({
+            'status': 'OK',
+            'code': 200,
+            'results': emailsvalidados})
+   
 
 if __name__ == "__main__":
     app.run()

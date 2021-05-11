1a250dad925fbb9b756c5776e0a074048ffddd50
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-05-11 00:18:37 -0300

    Update Banco_Dados.sql

--- Codigo_HTTP_Banco_Dados/Banco_Dados.sql
+++ Codigo_HTTP_Banco_Dados/Banco_Dados.sql
@@ -8,3 +8,19 @@ valid_syntax bit NULL,
 ID ASC
 )
 )
+
+CREATE TABLE ValidacaoV3(
+ID int IDENTITY(1,1) NOT NULL,
+email_adress varchar(50) NULL,
+domain varchar(50) NULL,
+valid_syntax bit NULL,
+disposable bit NULL,
+webmail bit NULL,
+deliverable bit NULL,
+catch_all bit NULL,
+gibberish bit NULL,
+ CONSTRAINT [PK__ValidacaoV3] PRIMARY KEY CLUSTERED
+(
+ID ASC
+)
+)
